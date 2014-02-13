c26c3882862d9e99e2a35cc9d30c96849e3e9c5d
aj0strow <alexander.ostrow@gmail.com>
2014-02-13 05:15:46 -0500

    skeletons

--- client/scripts/boot.js
+++ client/scripts/boot.js
@@ -22,10 +22,4 @@ $(function () {
   });
 
   Backbone.history.start({ pushState: true });
-
-  $('a.menu').click(function (ev) {
-    ev.preventDefault();
-    var path = $(ev.target).attr('href');
-    Backbone.history.navigate(path, true);
-  });
 });
