c01aa8d6dfa115d4c5ad7c8eaf2aa1d85f2abf56
AJ Ostrow <alexander.ostrow@gmail.com>
2016-01-18 16:32:09 -0500

    remove new relic

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var env = process.env['NODE_ENV']
 var port = process.env['PORT']
 
-if (env == 'production') {
-  require('newrelic')
-}
-
 var app = require('./src/index')
 app.listen(port || 8000)
