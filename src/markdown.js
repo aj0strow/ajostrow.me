afd1646cdfa78d930746cd23f0b59792d3831d2a
AJ Ostrow <alexander.ostrow@gmail.com>
2016-05-10 13:09:36 -0400

    add ids not that they work anyway

--- src/markdown.js
+++ src/markdown.js
@@ -3,10 +3,28 @@ var fs = bluebird.promisifyAll(require('fs'))
 var marked = bluebird.promisifyAll(require('marked'))
 var highlight = require('highlight.js')
 
+// auto include heading ids
+
+var renderer = new marked.Renderer()
+
+function quote (s) {
+  return '"' + s + '"'
+}
+
+renderer.heading = function (text, level) {
+  var tag = "h" + level
+  var anchor = text.toLowerCase().replace(/[^\w]+/g, '-')
+  return "<" + tag + " id=" + quote(anchor) + ">" + text + "</" + tag + ">"
+}
+
+// render markdown
+
 marked.setOptions({
   highlight: function (code) {
     return highlight.highlightAuto(code).value
-  }
+  },
+  
+  renderer: renderer,
 })
 
 function renderFile (file) {
